f732ad27c8d229fc6f96281bacba8859d898777a
Subhadip Maity <subhadipmaity09@gmail.com>
2024-02-15 22:40:28 +0530

    Custom Exception

---
+++ ANP_C7437_class5/src/feb12/InvalidAgeException.java
@@ -0,0 +1,8 @@
+package feb12;
+
+public class InvalidAgeException extends Exception {
+	public InvalidAgeException(String str)
+	{
+		super(str);
+	}
+}
---
+++ ANP_C7437_class5/src/feb12/Vote.java
@@ -0,0 +1,27 @@
+package feb12;
+
+public class Vote {
+	public static void validate(int age) throws InvalidAgeException
+	{
+		if(age < 18)
+		{
+			throw new InvalidAgeException("Age is not valid to vote");
+		}
+		else
+		{
+			System.out.println("Welcome to vote");
+		}
+	}
+	
+	public static void main(String[] args) {
+		try
+		{
+			validate(13);
+		}catch(InvalidAgeException ex)
+		{
+			System.out.println("Caught the exception");
+			System.out.println("Exception occured: "+ ex);
+		}
+		System.out.println("rest of the code..");
+	}
+}
