9490b3a532dc38a102b97001c202eef060cb20ec
Subhadip Maity <subhadipmaity09@gmail.com>
2024-02-15 23:29:59 +0530

    lab2-inheritance

---
+++ ANP_C7437_lab/src/lab2_feb15/Employee.java
@@ -0,0 +1,25 @@
+package lab2_feb15;
+
+public class Employee extends User {
+
+	double salary;
+	
+	Employee(int id, String name, double salary) {
+		super(id, name);
+		// TODO Auto-generated constructor stub
+		this.salary = salary;
+	}
+	
+	double calculateAnnualSalary()
+	{
+		return salary*12;
+	}
+	
+	public static void main(String[] args) {
+		Employee obj = new Employee(10, "Subhadip", 10000);
+		System.out.println("The annual salary of "+ obj.name + " is " + obj.calculateAnnualSalary());
+	}
+	
+	
+	
+}
---
+++ ANP_C7437_lab/src/lab2_feb15/User.java
@@ -0,0 +1,12 @@
+package lab2_feb15;
+
+public class User {
+	int id;
+	String name;
+	
+	User(int id, String name)
+	{
+		this.id = id;
+		this.name = name;
+	}
+}
