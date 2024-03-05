628e7a06eb8d532f1131c7bf2c93f956b22ce5f7
Subhadip Maity <subhadipmaity09@gmail.com>
2024-03-06 00:10:41 +0530

    jdbc

--- ANP_C7437_JDBC_class16/src/march5jdbc/Demo.java
+++ ANP_C7437_JDBC_class16/src/march5jdbc/Demo.java
@@ -4,8 +4,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Demo {
+	private static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
 //		there are 5 steps to connect database with java
 //		1st step
@@ -20,9 +22,23 @@ public class Demo {
 		String username = "root";
 		String password = "Subha@2001";
 		try {
+			
 			Connection con = DriverManager.getConnection(url, username, password);
+			System.out.println("enter your student id ");
+			int stuid = sc.nextInt();
+			System.out.println("enter student name ");
+			String stuname = sc.next();
+			System.out.println("enter student address ");
+			String stuaddress = sc.next();
 //		3rd Step: Create database object
-			PreparedStatement ps =  con.prepareStatement("insert into Student values(5656, 'Kali', 'Pitampur')");
+			// by doing next line you have to change the query again n again
+//			PreparedStatement ps = con.prepareStatement("insert into Student values(5656, 'Kali', 'Pitampur')");
+			// another way to write this using position parameter
+			
+			PreparedStatement ps = con.prepareStatement("insert into Student values(?, ?, ?)");
+			ps.setInt(1, stuid);
+			ps.setString(2, stuname);
+			ps.setString(3, stuaddress);
 			//when we updating data
 			int row = ps.executeUpdate();
 			
