6934d766f2915b8cc1f1b5c81221ddf9d044eff5
Subhadip Maity <subhadipmaity09@gmail.com>
2024-03-05 23:14:07 +0530

    jdbc

--- ANP_C7437_JDBC_class16/src/march5jdbc/Demo.java
+++ ANP_C7437_JDBC_class16/src/march5jdbc/Demo.java
@@ -2,6 +2,7 @@ package march5jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Demo {
@@ -21,8 +22,15 @@ public class Demo {
 		try {
 			Connection con = DriverManager.getConnection(url, username, password);
 //		3rd Step: Create database object
-			 
-		} catch (SQLException e) {
+			PreparedStatement ps =  con.prepareStatement("insert into Student values(5656, 'Kali', 'Pitampur')");
+			//when we updating data
+			int row = ps.executeUpdate();
+			
+			if(row > 0)
+			{
+				System.out.println("data get inserted successfully");
+			}
+;		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
