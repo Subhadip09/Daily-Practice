e9b7a3541aa653f370e3996c717c9d6fb41c0364
Subhadip Maity <subhadipmaity09@gmail.com>
2024-03-14 22:13:59 +0530

    quiz

--- ANP_C7437_lab/src/quiz/Test1.java
+++ ANP_C7437_lab/src/quiz/Test1.java
@@ -2,38 +2,12 @@ package quiz;
 
 import java.util.*;
 
-class Test1 implements Runnable {
-
-static Thread t1, t2;
-
-public void run() {
-
-for(int i=0; ;){
-
-System.out.println(Thread.currentThread().getName());
-
-i++;
-
-}
-
-}
-
-public static void main(String ar[]) {
-
-Test1 t=new Test1();
-
-t1=new Thread(t, "T1");
-
-t2=new Thread(t, "T2");
-
-try{ t1.start();
-
-t1.sleep(5000);
-
-}catch(Exception e){ }
-
-t2.start();
-
-}
-
+class Test1 {
+	public static void main(String[] args) {
+		TreeSet<Integer> set = new TreeSet<>();
+		set.add(24);
+		set.add(5);
+		set.add(50);
+		System.out.println(set.first());
+		}
 }
